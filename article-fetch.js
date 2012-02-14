9757ff60530cc2bea7c664a969cdbbc02cf0e39e
Andris Reinman <andris@node.ee>
2012-02-14 15:48:12 +0200

    update

--- article-fetch.js
+++ article-fetch.js
@@ -54,8 +54,9 @@ function sanitizeHTML(html){
     html = html.trim().
         replace(/\r?\n|\r/g, "\u0000").
         replace(/<(div|p|ul|li|h1|h2|h3|h4|h5)>[\s\u0000]*(<br>[\s\u0000]*)*/g, "<$1>").
-        replace(/(?:[\s\u0000]*<br>)*[\s\u0000]*<\/(div|p|ul|li|h1|h2|h3|h4|h5)>/g, "<$1>")
-        replace(/\u0000/g, "\n");
+        replace(/(?:[\s\u0000]*<br>)*[\s\u0000]*<\/(div|p|ul|li|h1|h2|h3|h4|h5)>/g, "<$1>").
+        replace(/\u0000/g, "\n").
+        trim();
     
     return html;
 }
