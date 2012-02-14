8250c2c6a204a1d494500da7700ae7fe142b973d
Andris Reinman <andris@node.ee>
2012-02-14 16:31:06 +0200

    update

--- article-fetch.js
+++ article-fetch.js
@@ -54,7 +54,7 @@ function sanitizeHTML(html){
         replace(/\r?\n|\r/g, "\u0000").
         replace(/<(div|p|ul|li|h1|h2|h3|h4|h5)>[\s\u0000]*(<br>[\s\u0000]*)*/g, "<$1>").
         replace(/(?:[\s\u0000]*<br>)*[\s\u0000]*<\/(div|p|ul|li|h1|h2|h3|h4|h5)>/g, "<$1>").
-        replace(/<\!\-\-.*\-\->/). //comments
+        replace(/<\!\-\-.*\-\->/g, ""). //comments
         replace(/<([A-Za-z][^> ]*)[^>]*>[\s\u0000]*<\/([A-Za-z][^> ]*)[^>]*>/g,function(original, startTag, endTag){
             startTag = (startTag || "").toLowerCase();
             endTag = (endTag || "").toLowerCase();
